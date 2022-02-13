8cb59fa7383b613129e9c0f4e55eb897a1450b72
MariBelniok <mariannabelniok@outlook.com>
2022-02-13 10:35:25 -0300

    slug started

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,9 +1,13 @@
+import { format } from 'date-fns';
+import { ptBR } from 'date-fns/locale';
 import { GetStaticPaths, GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../../services/prismic';
+import Prismic from '@prismicio/client';
 
 import commonStyles from '../../styles/common.module.scss';
 import styles from './post.module.scss';
+import { RichText } from 'prismic-dom';
 
 interface Post {
   first_publication_date: string | null;
@@ -26,20 +30,52 @@ interface PostProps {
   post: Post;
 }
 
-// export default function Post() {
-//   // TODO
-// }
+export default function Post({ post }: PostProps) {
+  return (
+    <>
+      <h1> oi </h1>
+    </>
+  )
+}
 
-// export const getStaticPaths = async () => {
-//   const prismic = getPrismicClient();
-//   const posts = await prismic.query(TODO);
+export const getStaticPaths = async () => {
+  const prismic = getPrismicClient();
+  const posts = await prismic.query([
+    Prismic.predicates.at('document.type', 'posts')
+  ], {
+    fetch: ['post.title', 'post.subtitle', 'post.author', 'post.banner', 'post.content'],
+  });
 
-//   // TODO
-// };
+  return {
+    paths: posts,
+    fallback: 'blocking'
+  }
+};
 
-// export const getStaticProps = async context => {
-//   const prismic = getPrismicClient();
-//   const response = await prismic.getByUID(TODO);
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const prismic = getPrismicClient();
+  const response: any = await prismic.getByUID('posts', String(slug), {});
 
-//   // TODO
-// };
+  const post: Post = {
+    first_publication_date: format(new Date(response.last_publication_date), 'dd MMM yyyy', { locale: ptBR }),
+    data: {
+      author: response.data.author,
+      banner: response.data.banner,
+      content: [{
+        heading: RichText.asHtml(response.data.content.heading),
+        body: [{
+          text: RichText.asHtml(response.data.content.text),
+        }]
+      }],
+      title: RichText.asText(response.data.title)
+    }
+  }
+
+  return {
+    props: {
+      post
+    },
+    revalidate: 60 * 30
+  }
+};
